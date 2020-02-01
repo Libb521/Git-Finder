2fc435a9b020c82ec5d70f4332ec955a0e6f9d43
Libb521 <eotieno39@yahoo.com>
2020-02-01 22:10:02 +0300

    created the fetch functions giving them parameters to search for when looking for profiles and repos

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfService} from '../services/prof.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profile: any;
+  repos: any;
+  username: string;
+
+  // profile = any;
+  // { login: '',
+  //   company: '',
+  //   location: '',
+  //   avatar_url: '',
+  //   public_repos: '',
+  //   public_gist: '',
+  //   followers: '',
+  //   following: '',
+  //   email: '',
+  //   bio: '',
+  //   created_at: '',};
+  // repos: any;
+  // username: string;
+
+  constructor(private service: ProfService) { 
+    this.service.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+    this.service.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+  }
+
+  // findProfile(){
+  //   this.service.updateProfile(this.username);
+
+  //   this.service.getProfileInfo().subscribe(profile => {
+  //     console.log(profile);
+  //     this.profile = profile;
+  //   });
+  //   this.service.getProfileRepos().subscribe(repos =>{
+  //     console.log(repos);
+  //     this.repos = repos;
+  //   });
+  // }
 
   ngOnInit() {
+    this.username = "Libb521"
+    this.service.getProfileInfo().subscribe(profile => this.profile =profile);
+    this.service.getProfileRepos().subscribe(repos => this.repos =repos);
   }
 
 }
