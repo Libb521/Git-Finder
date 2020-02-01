af6d9a6ab18727802e5c6526be42a78abe172ace
Libb521 <eotieno39@yahoo.com>
2020-02-01 15:11:56 +0300

    imported the HttpClient from the angular module into the prof.service.ts, also added the public apiKey into the file so that we can be able to search through github afew times looking for different users

--- src/app/services/prof.service.ts
+++ src/app/services/prof.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, Headers} from 'angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfService {
+  
+  public username: string;
+  public apiKey: '85493ea390c295a8a5f18d4f337a163eabb0c17f';
 
-  constructor() { }
+
+  constructor(public http:HttpClient) {
+    console.log("service ready for use!");
+    this.username = 'Libb521';
+   }
 }
