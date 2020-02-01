6751239732dd17b7444e0428f3636009e6af809a
Libb521 <eotieno39@yahoo.com>
2020-02-01 18:15:04 +0300

    imported the Httpclient module into the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { ProfService} from './services/prof.service';
 
 import { AppRoutingModule } from './app-routing.module';
